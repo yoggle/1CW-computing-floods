bf6d037714230afe1be8380b8021c257993ca89e
Yoggle Dranyam <thomas.maynard.2000@gmail.com>
2025-02-05 21:25:56 +0000

    fixed output of Task1B

--- Task1B.py
+++ Task1B.py
@@ -1,15 +1,19 @@
-from floodsystem.geo import stations_by_distance
+from floodsystem.geo import stations_by_distance, haversine
 from floodsystem.stationdata import build_station_list
+
 
 def run():
 
     """ Take a list of stations and sort them by distance from a given coordinate with a function stored in geo"""
 # sort stations that are imported by build_station_list
     sorted_stations = stations_by_distance(build_station_list(),(52.2053, 0.1218))
+    toprint =[]
+    for i in sorted_stations:
+        toprint.append((i.name,i.river, haversine(origin = (52.2053, 0.1218),new = i.coord)))
     
     # print closest 10 then furthest 10 stations from the centre of cambridge
-    print("The closest ten stations are: ",sorted_stations[:10])
-    print("The furthest 10 stations are: ",sorted_stations[10:])
+    print("The closest ten stations are: ",toprint[:10])
+    print("The furthest 10 stations are: ",toprint[-10:])
     
     #return sorted list of stations
     return(sorted_stations)
