5b49d97388623d453cf69a8ac77e97f21f9d2060
Yoggle Dranyam <thomas.maynard.2000@gmail.com>
2025-02-25 21:38:14 +0000

    2E test

---
+++ test_plot.py
@@ -0,0 +1,4 @@
+def test_plot_water_levels():
+    from floodsystem.plot import plot_water_levels
+    from floodsystem.stationdata import build_station_list
+    plot_water_levels(build_station_list[1],(2,5,6,3,6),(1,2,3,4,5))
