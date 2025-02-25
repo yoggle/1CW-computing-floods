cda69bc1d0e958998923f4eb976163d07fc70102
Yoggle Dranyam <thomas.maynard.2000@gmail.com>
2025-02-25 21:31:59 +0000

    Task 2E without full demo programme

--- Task2E.py
+++ Task2E.py
@@ -1,5 +1,14 @@
 from floodsystem.plot import plot_water_levels
-from datafetcher import 
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.stationdata import build_station_list
 import datetime
 
-print(fetch_dat)
+stationlist = build_station_list()
+
+dt = 50
+
+dates, levels = fetch_measure_levels(stationlist[5].measure_id
+                           
+                        , dt = datetime.timedelta(days = dt))
+
+plot_water_levels(stationlist[5],dates,levels)
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,2 +1,17 @@
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+from floodsystem.datafetcher import fetch_measure_levels
+
 def plot_water_levels(station, dates,levels):
-    
+    plt.plot(dates,levels, label = "Water Level")
+    plt.xlabel("Date")
+    plt.ylabel("Water Level (m)")
+    plt.xticks(rotation = 45)
+    plt.title(f"Station {station.name}")
+
+    plt.plot(dates,[station.typical_range[1] for i in dates], color = "red", label = "Typical High")
+    plt.plot(dates,[station.typical_range[0] for i in dates], color = "black", label = "Typical Low")
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+    return 0
