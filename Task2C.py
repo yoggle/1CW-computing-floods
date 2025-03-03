563f6ccc140d4e6b13407c49d4f3c8110cbc5a5e
Justinzz94 <zz510@cam.ac.uk>
2025-03-03 11:07:08 +0000

    Update Task2C.py

--- Task2C.py
+++ Task2C.py
@@ -1,21 +1,8 @@
-from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_highest_rel_level
+from floodsystem.stationdata import build_station_list, update_water_levels
 
-
-def run():
-    
-
-    stations = build_station_list()
-    update_water_levels(stations)
-
-
-    stations = stations_highest_rel_level(stations, 10)
-    for i in stations:
-        print(i.name, i.relative_water_level())
-
-
-
-
-if __name__ == "__main__":
-    print("*** Task 2C: CUED Part IA Flood Warning System ***")
-    run()
+stations = build_station_list()
+update_water_levels(stations)
+stations_highest_rel_level = stations_highest_rel_level(stations, 10)
+for station, relative_level in stations_highest_rel_level:
+    print(f"{station.name}  {relative_level}")
