63de1c1d979652ed92b9aa80d219a6df99c2f26e
Justinzz94 <zz510@cam.ac.uk>
2025-03-03 11:08:58 +0000

    Update Task2B.py

--- Task2B.py
+++ Task2B.py
@@ -1,22 +1,10 @@
+from floodsystem.flood import stations_level_over_threshold
 from floodsystem.stationdata import build_station_list, update_water_levels
-from floodsystem.station import stations_level_over_threshold
+
+stations = build_station_list()
+update_water_levels(stations)
 
 
-def run():
-    
-    
-    stations = build_station_list()
-    update_water_levels(stations)
-
-    threshold = 0.8
-    stations_over_threshold = stations_level_over_threshold(stations, threshold)
-
-    for station, level in stations_over_threshold:
-        print(f"{station.name} {level:.6f}") 
-
-
-
-
-if __name__ == "__main__":
-    print("*** Task 2B: CUED Part IA Flood Warning System ***")
-    run()
+stations_over_threshold = stations_level_over_threshold(stations, 0.8)
+for station, relative_level in stations_over_threshold:
+    print(f"{station.name}  {relative_level}")
