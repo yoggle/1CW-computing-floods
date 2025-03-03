0797feb82b1e77093bf37df523fb0f57c84d790c
Justinzz94 <zz510@cam.ac.uk>
2025-03-03 11:08:33 +0000

    Update flood.py

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,7 +1,24 @@
+from floodsystem.stationdata import build_station_list, update_water_levels
+
+
+stations = build_station_list()
+update_water_levels(stations)
+
+def stations_level_over_threshold(stations, tol):
+    stations_over_threshold = []
+    stations_over_threshold_none = []
+    for station in stations:
+        if station.typical_range is None:
+            stations_over_threshold_none.append((station.name, relative_level))
+        relative_level = station.relative_water_level()
+        if relative_level is not None and relative_level > tol:
+            stations_over_threshold.append((station.name, relative_level))
+    return sorted(stations_over_threshold, key=lambda x:x[1], reverse=True)
+
 def stations_highest_rel_level(stations, N):
-    '''Returns the first N stations with the highest relative water level'''
-    stations = sorted(stations, key=lambda x: x.relative_water_level() or -float('inf'), reverse=True)
-
-    toReturn = stations[:N]
-
-    return(toReturn)
+    relative_water_level = []
+    for station in stations:
+        relative_level = station.relative_water_level()
+        if relative_level is not None:
+            relative_water_level.append((station, relative_level))
+    return sorted(relative_water_level, key=lambda x:x[1], reverse=True)[:N]
