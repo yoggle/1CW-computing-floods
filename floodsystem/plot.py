78996b12c7b50650d94989cf8d95fe85ec478296
Justinzz94 <zz510@cam.ac.uk>
2025-03-03 11:03:40 +0000

    Update plot.py

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 from floodsystem.datafetcher import fetch_measure_levels
+import numpy as np
 
 def plot_water_levels(station, dates,levels):
     plt.plot(dates,levels, label = "Water Level")
@@ -15,3 +16,22 @@ def plot_water_levels(station, dates,levels):
     plt.tight_layout()
     plt.show()
     return 0
+
+
+def plot_water_level_with_fit(station, dates, levels, p):
+    poly, d0 = polyfit(dates, levels, p)
+    time = mdates.date2num(dates)
+    time = time - d0
+    plt.plot(dates, levels, label='actual water level')
+    plt.plot(dates, poly(time), label='fitted polynomial')
+    plt.xlabel('date')
+    plt.ylabel('water level (m)')
+    upper=[station.typical_range[1] for i in range(len(dates))]
+    lower=[station.typical_range[0] for i in range(len(dates))]
+    plt.plot(dates, upper, label='typical high',linestyle='--')
+    plt.plot(dates, lower, label='typical low',linestyle='--')
+    plt.title('station: '+station.name)
+    plt.legend()
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
